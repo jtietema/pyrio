756bd439fd3c5294b2cb60603b0255c6216dd68b
Maik Gosenshuis <maik@gosenshuis.nl>
2009-02-04 23:50:43 +0100

    Map now moves with keyboard controls (no boundaries yet)

--- map.py
+++ map.py
@@ -1,16 +1,78 @@
 
 import pygame
+from pygame.locals import *
 import os
 
-class Map():
+class Map():    
     def __init__(self, player):
         self.player = player
-        self.groundUp = pygame.image.load(os.path.join('assets', 'ground', 'up.png')).convert()
+        self.speed = .3
+        self.x_offset = 0
+        
+        self.tiles = (
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png',
+            'up.png'
+        )
+        
+        self.tile_images = {
+            'up.png': pygame.image.load(os.path.join('assets', 'ground', 'up.png')).convert()
+        }
         
     def update(self, tick_data):
-        pass
+        x_delta = tick_data['time_passed'] * self.speed
+        
+        if tick_data['pressed_keys'][K_LEFT]:
+            self.x_offset += x_delta
+        if tick_data['pressed_keys'][K_RIGHT]:
+            self.x_offset -= x_delta
         
     def render(self, screen):
-        for i in range(13):
-            screen.blit(self.groundUp, (i*64, 400))
+        y = screen.get_height() - 64
+        
+        screen_width = screen.get_width()
+        
+        tile_width = 64
+        total_width = len(self.tiles) * tile_width
+        
+        x = self.x_offset
+        
+        for (index, tile_filename) in enumerate(self.tiles):
+            if x + tile_width > 0 or x < screen_width:
+                tile_image = self.tile_images[tile_filename]
+                screen.blit(tile_image, (x, y))
+            x += tile_width
         
