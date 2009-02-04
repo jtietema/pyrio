8f952f0e0fdc386d90a88300b552a7b5c1ad9850
Maik Gosenshuis <maik@gosenshuis.nl>
2009-02-04 15:57:53 +0100

    Moved main game loop to Game class

--- game.py
+++ game.py
@@ -1,4 +1,8 @@
 
+import pygame
+from pygame.locals import *
+from sys import exit
+
 from world import World
 
 class Game():
@@ -10,3 +14,28 @@ class Game():
         
     def render(self, screen):
         self.world.render(screen)
+    
+    
+    @classmethod
+    def run(cls):
+        pygame.init()
+
+        screen = pygame.display.set_mode((800, 600), 0, 32)
+        pygame.display.set_caption("Pygame platform")
+
+        clock = pygame.time.Clock()
+        
+        game = Game()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    exit()
+
+            time_passed = clock.tick()
+
+            game.update(time_passed)
+            game.render(screen)
+
+            pygame.display.update()
+        
