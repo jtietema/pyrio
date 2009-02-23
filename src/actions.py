8a8d44b7d582bef2b8e3e242953ea5274f996d0f
jeroen <unknown>
2009-02-23 16:32:47 +0100

    moved process_controls to actions object moved joystick init to actions object

--- src/actions.py
+++ src/actions.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import pygame
+from pygame.locals import *
+
 class Actions():
     """
     Simple class to hold all the actions the player can do.  In the rest of the game you
@@ -10,6 +13,17 @@ class Actions():
         self.jump = False
         self.select = False
         self.cancel = False
+        self.joystick = None
+        
+        # setup joystick/gamepad if present
+        joystick = None
+        if pygame.joystick.get_count() > 0:
+            # at least one joystick is found
+            print str(pygame.joystick.get_count()) + ' joystick(s) found'
+            self.joystick = pygame.joystick.Joystick(0)
+            self.joystick.init()
+            print 'Joystick found with ' + str(self.joystick.get_numaxes()) + \
+             ' axes and ' + str(self.joystick.get_numbuttons()) + ' buttons'
 
     def set_x(self, x):
         """Must be a float between 1 (right) and -1 (left)"""
@@ -30,3 +44,45 @@ class Actions():
     
     def set_cancel(self, boolean):
         self.cancel = True
+    
+    def process_controls(self):
+        """
+        Maps all the supported controls to a common format.
+        """
+        self.reset()
+        # process the keyboard controls
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_UP]:
+            self.set_jump(True)
+
+        if pressed_keys[K_LEFT]:
+            self.set_x(-1.0)
+
+        if pressed_keys[K_RIGHT]:
+            self.set_x(1.0)
+
+        if pressed_keys[K_UP]:
+            self.set_y(1.0)
+
+        if pressed_keys[K_DOWN]:
+            self.set_y(-1.0)
+
+        if pressed_keys[K_RETURN]:
+            self.set_select(True)
+        
+        if pressed_keys[K_ESCAPE]:
+            self.set_cancel(True)
+
+        # process gamepad / joystick
+        if self.joystick is not None:
+            if self.joystick.get_button(0):
+                self.set_jump(True)
+            if self.joystick.get_button(1):
+                self.set_select(True)
+            if self.joystick.get_axis(0) > .05 or self.joystick.get_axis(0) < -.05:
+                self.set_x(self.joystick.get_axis(0))
+            if self.joystick.get_axis(1) > .05 or self.joystick.get_axis(1) < -.05:
+                self.set_y(-1 * self.joystick.get_axis(1))
+        
+        return self
+        
--- src/game.py
+++ src/game.py
@@ -55,15 +55,6 @@ class Game():
 
         actions = Actions()
 
-        # setup joystick/gamepad if present
-        joystick = None
-        if pygame.joystick.get_count() > 0:
-            # at least one joystick is found
-            print str(pygame.joystick.get_count()) + ' joystick(s) found'
-            joystick = pygame.joystick.Joystick(0)
-            joystick.init()
-            print 'Joystick found with ' + str(joystick.get_numaxes()) + ' axes and ' + str(joystick.get_numbuttons()) + ' buttons'
-
         while True:
             tick_data = {}
 
@@ -83,7 +74,7 @@ class Game():
             # Default values for tick data items.
             time_passed = clock.tick()
             tick_data['time_passed'] = time_passed
-            tick_data['actions'] = self.process_controls(actions, joystick)
+            tick_data['actions'] = actions.process_controls()
             tick_data['screen_size'] = screen.get_size()
             tick_data['score'] = self.score
             tick_data['lives'] = self.lives
@@ -132,47 +123,6 @@ class Game():
             
     def reset_world(self):
         self.world = self.map_package.current()
-
-    def process_controls(self, actions, joystick):
-        """
-        Maps all the supported controls to a common format.
-        """
-        actions.reset()
-        # process the keyboard controls
-        pressed_keys = pygame.key.get_pressed()
-        if pressed_keys[K_UP]:
-            actions.set_jump(True)
-
-        if pressed_keys[K_LEFT]:
-            actions.set_x(-1.0)
-
-        if pressed_keys[K_RIGHT]:
-            actions.set_x(1.0)
-
-        if pressed_keys[K_UP]:
-            actions.set_y(1.0)
-
-        if pressed_keys[K_DOWN]:
-            actions.set_y(-1.0)
-
-        if pressed_keys[K_RETURN]:
-            actions.set_select(True)
-        
-        if pressed_keys[K_ESCAPE]:
-            actions.set_cancel(True)
-
-        # process gamepad / joystick
-        if joystick is not None:
-            if joystick.get_button(0):
-                actions.set_jump(True)
-            if joystick.get_button(1):
-                actions.set_select(True)
-            if joystick.get_axis(0) > .05 or joystick.get_axis(0) < -.05:
-                actions.set_x(joystick.get_axis(0))
-            if joystick.get_axis(1) > .05 or joystick.get_axis(1) < -.05:
-                actions.set_y(-1 * joystick.get_axis(1))
-        
-        return actions
 
     def get_lives(self):
         return self.lives
